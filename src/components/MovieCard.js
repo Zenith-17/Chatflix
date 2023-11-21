dc00a87bc75bdf909c68cc14f27aa873ceb849ac
Atharva Bhatnagar <99113156+Zenith-17@users.noreply.github.com>
2023-11-21 10:02:41 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { IMG_CDN_URL } from '../utils/constants'
+import { Link } from 'react-router-dom';
+// import MovieDetailsPage from './MovieDetailsPage';
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({posterPath,id}) => {
   if(!posterPath)
   return null;
+  // const showMovieDeatils=()=>{
+  //   // console.log(id);
+  //   <MovieDetailsPage movieId={id} />
+  // }
   return (
-    <div className='w-36 md:w-48 pr'>
+    <div className='w-36 md:w-48 cursor-pointer hover:scale-105 transition-transform'>
+        <Link to={"/browse/" + id}>
         <img src={IMG_CDN_URL+posterPath} alt="Movie Card" />
+        </Link>
+        
     </div>
   )
 }
 
 export default MovieCard
+
+
+ 
