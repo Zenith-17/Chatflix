ec7c9d7865642e8a624e2443ccb91f525f0e96a4
Atharva Bhatnagar <99113156+Zenith-17@users.noreply.github.com>
2023-11-21 10:02:28 +0530

    Update Maincontainer.js

--- src/components/Maincontainer.js
+++ src/components/Maincontainer.js
@@ -4,18 +4,19 @@ import VideoTitle from './VideoTitle';
 import VideoBackground from './VideoBackground';
 
 const Maincontainer = () => {
-    const movies=useSelector(store=>store.movies?.nowPlayingMovies);
+    const movies=useSelector(store=>store.movies?.popularMovies);
     
     if(!movies)
     return "cannot fetch movies";
 
-    const mainMovie=movies[0];
-
+    const mainMovie=movies[8];
+  
     const {original_title,overview,id}=mainMovie;
-
+  
     return (
     <div className='md:pt-0 pt-[20%] bg-black'>
         <VideoTitle title={original_title} overview={overview}/>
+    
         <VideoBackground movieId={id}/>
     </div>
   )
