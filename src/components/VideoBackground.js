e37988dc9dfffe189ba3c217d8636da4ba4efe3c
Atharva Bhatnagar <99113156+Zenith-17@users.noreply.github.com>
2023-11-21 10:04:05 +0530

    Update VideoBackground.js

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,24 +1,25 @@
-import { useSelector } from 'react-redux'
-import useMovieTrailer from '../hooks/useMovieTrailer';
+import useMovieTrailer from '../hooks/useMovieTrailer'
+import { useSelector } from "react-redux";
 
-const VideoBackground = ({movieId}) => {
-    const trailerVideo=useSelector(store=>store.movies?.tralerVideo);
-    useMovieTrailer(movieId);    
-  return (
-    <div>
-        <iframe 
-        className='w-screen aspect-video'
-        width="560" 
-        height="315" 
-        src={
-          "https://www.youtube.com/embed/"+
-          trailerVideo?.key+
-        "?&autoplay=1&mute=1"
-        }
-         title="YouTube video player" 
-         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-    </div>
-  )
+const VideoBackground = ({ movieId }) => {
+  // console.log(movieId);
+     useMovieTrailer(movieId);
+    const trailerObj = useSelector(store => store.movies?.trailerVideo);
+   
+    return (
+        <div className='sm:pt-0 pt-16'>
+            <iframe
+                className="w-full aspect-video  "
+                src={
+                    "https://www.youtube.com/embed/" +
+                    trailerObj?.key +
+                    "?&autoplay=1&loop=1&mute=1"
+                }
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+        </div>
+    )
 }
 
-export default VideoBackground
+export default VideoBackground;
